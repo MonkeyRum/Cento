f97c1d765d8804aa793ebc2cb8c9f37e40c3548a
MonkeyRum <monkey.rum.github@gmail.com>
2015-02-05 19:37:46 +0000

    Added beginning of GridImageBox
    See #2

--- Cento.Control/GridImageBox.cs
+++ Cento.Control/GridImageBox.cs
@@ -10,11 +10,148 @@ using System.Windows.Forms;
 
 namespace Cento.Control
 {
+    /// <summary>
+    /// Control for displaying an image with a grid overlay.
+    /// Can return grid index on mouse click.
+    /// </summary>
     public partial class GridImageBox : UserControl
     {
+        #region Members
+
+        private readonly Point ORIGIN = new Point(0, 0);
+
+        private Image _image = null;
+        private float _imageScale = 1.0f;
+        private bool _scaleToFit = false;
+
+        #endregion
+
+        #region Constructors
+
         public GridImageBox()
         {
             InitializeComponent();
+
+            this.SetStyle(
+                ControlStyles.ResizeRedraw | 
+                ControlStyles.OptimizedDoubleBuffer | 
+                ControlStyles.UserPaint |
+                ControlStyles.AllPaintingInWmPaint, true);
+
+            // TEMP
+            this.Image = Image.FromFile(@"F:\backup\Pictures\Imagedataset\IMG_5097.JPG");
+
+            // Set false in member declare and true here to force scale update
+            this.ScaleToFit = true;
         }
+
+        #endregion
+
+        #region Properties
+
+        [DefaultValue(typeof(Image), null)]
+        public Image Image
+        {
+            get
+            {
+                return this._image;
+            }
+            set
+            {
+                if (!ReferenceEquals(value, this._image))
+                {
+                    this._image = value;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        [DefaultValue(typeof(float), "1.0f")]
+        public float ImageScale
+        {
+            get
+            {
+                return this._imageScale;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    this._imageScale = value;
+                }
+            }
+        }
+
+        [DefaultValue(typeof(bool), "true")]
+        public bool ScaleToFit
+        {
+            get
+            {
+                return this._scaleToFit;
+            }
+            set
+            {
+                if (value != this._scaleToFit)
+                {
+                    this._scaleToFit = value;
+                    this.ScaleToFitImpl();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Scales the image to fit within the bounds of the control.
+        /// Will fire a ScaleChanged event if the scale has changed.
+        /// </summary>
+        private void ScaleToFitImpl()
+        {
+            // image dimensions
+            int iw = this.Image.Width;
+            int ih = this.Image.Height;
+
+            // control dimensions
+            int cw = this.Width;
+            int ch = this.Height;
+
+            // scale factors
+            float sw = (float)cw / iw;
+            float sh = (float)ch / ih;
+
+            this.ImageScale = sw <= sh ? sw : sh;
+        }
+
+        #endregion
+
+        #region Overrides
+
+        protected override void OnResize(EventArgs e)
+        {
+            if (this.ScaleToFit)
+            {
+                this.ScaleToFitImpl();
+            }
+
+            base.OnResize(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            var g = e.Graphics;
+
+            if (this.Image != null)
+            {
+                g.ResetTransform();
+                g.ScaleTransform(this.ImageScale * 2, this.ImageScale * 2);
+                g.DrawImage(this.Image, ORIGIN);
+            }
+        }
+
+        #endregion
     }
 }
--- Cento/MainView.Designer.cs
+++ Cento/MainView.Designer.cs
@@ -28,26 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainView));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.gridImageBox1 = new Cento.Control.GridImageBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolLabelScaleFactor = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.projectToolStripMenuItem,
+            this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(624, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openProjectToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolLabelScaleFactor});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // gridImageBox1
+            // 
+            this.gridImageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gridImageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridImageBox1.Image = ((System.Drawing.Image)(resources.GetObject("gridImageBox1.Image")));
+            this.gridImageBox1.ImageScale = 0.1360677F;
+            this.gridImageBox1.Location = new System.Drawing.Point(3, 3);
+            this.gridImageBox1.Name = "gridImageBox1";
+            this.gridImageBox1.Size = new System.Drawing.Size(418, 364);
+            this.gridImageBox1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(624, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.Controls.Add(this.gridImageBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.propertyGrid1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 49);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(624, 370);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid1.Location = new System.Drawing.Point(427, 3);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(194, 364);
+            this.propertyGrid1.TabIndex = 3;
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // toolLabelScaleFactor
+            // 
+            this.toolLabelScaleFactor.Name = "toolLabelScaleFactor";
+            this.toolLabelScaleFactor.Size = new System.Drawing.Size(118, 17);
+            this.toolLabelScaleFactor.Text = "toolStripStatusLabel1";
+            // 
+            // openProjectToolStripMenuItem
+            // 
+            this.openProjectToolStripMenuItem.Name = "openProjectToolStripMenuItem";
+            this.openProjectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openProjectToolStripMenuItem.Text = "&Open project";
+            // 
+            // projectToolStripMenuItem
+            // 
+            this.projectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addImageToolStripMenuItem});
+            this.projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            this.projectToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.projectToolStripMenuItem.Text = "&Project";
+            // 
+            // addImageToolStripMenuItem
+            // 
+            this.addImageToolStripMenuItem.Enabled = false;
+            this.addImageToolStripMenuItem.Name = "addImageToolStripMenuItem";
+            this.addImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addImageToolStripMenuItem.Text = "&Add Image";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainView";
-            this.Text = "Form1";
+            this.Text = "Cento";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +200,20 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private Control.GridImageBox gridImageBox1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolLabelScaleFactor;
+        private System.Windows.Forms.ToolStripMenuItem openProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem projectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addImageToolStripMenuItem;
     }
 }
 
